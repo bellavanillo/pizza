2a3269a92cd5957373717c5ec462dc13cf4c10e1
bv <bellavanillo@gmail.com>
2018-03-14 18:31:53 -0700

    add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
+//Biz logic
+var callFunction = function() {
+  ???? hold the numbers here
+  for (var i=1; i<=number; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log("pingpong");
+    }
+    else if ( i % 3 === 0 ) {
+      console.log("ping");
+    }
+    else if (i % 5 ===0){
+      console.log("pong");
+    }
+    else {
+      console.log(i);
+    }
+  }
+  ?????
+}
+
 $(document).ready(function(){
   $("form.form1").submit(function(event){
     event.preventDefault();
-    for (var i=1; i<=number; i++) {
-    if (i % 3 === 0 && i % 5 === 0){
-    console.log("pingpong");
-    }
-    else if ( i % 3 === 0 ) {
-    console.log("ping");
-    }
-    else if (i % 5 ===0){
-    console.log("pong");
-    }
-    else {
-    console.log(i);
-    }
-    }
+    var userInput = parseInt($(???)???);
+    var newName = callFunction(userInput);
+
+    display all the numbers with code here (.append();)
   });
 });
