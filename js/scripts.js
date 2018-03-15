683606708c472730344f2fe8ca4ef9283f019020
bv <bellavanillo@gmail.com>
2018-03-14 19:50:26 -0700

    add arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 //Biz logic
-var callFunction = function() {
-  ???? hold the numbers here
-  for (var i=1; i<=number; i++) {
-    if (i % 3 === 0 && i % 5 === 0){
-      console.log("pingpong");
-    }
-    else if ( i % 3 === 0 ) {
-      console.log("ping");
-    }
-    else if (i % 5 ===0){
-      console.log("pong");
-    }
-    else {
-      console.log(i);
+var callFunction = function(userInput){
+  var outputArray = [];
+  for (var i = 1; i <= userInput; i++){
+var index = outputArray.indexOf(i);
+    if (i % 15 === 0){
+      outputArray.slice(index, 0);
+      outputArray.push("pingpong");
+    }else if ( i % 3 === 0){
+      outputArray.slice(index, 0);
+      outputArray.push("ping");
+    }else if (i % 5 ===0){
+      outputArray.slice(index, 0);
+      outputArray.push("pong");
+    }else {
+      outputArray.push(i);
     }
   }
-  ?????
-}
+  alert ("outputArray");
+  //return outputArray;
+};
 
 $(document).ready(function(){
   $("form.form1").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($(???)???);
+    debugger
+    var userInput = parseInt($("input#number").val());
     var newName = callFunction(userInput);
 
-    display all the numbers with code here (.append();)
+    newName.forEach(function(value){
+      $("#output").append('<li>' + value + '</li>');
+    })
+    //display all the numbers with code here (.append();)
   });
 });
